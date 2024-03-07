b3bc2d3b61413ddcc632865d60d2966bd39f938a
André Oliveira <p32blo@gmail.com>
2024-03-07 08:43:13 +0000

    Add ssh configs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -48,6 +48,38 @@
       }
     ];
   };
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "qube*.local" = {
+        user = "qube";
+        extraOptions = {
+          StrictHostKeyChecking = "no";
+        };
+      };
+      "qube-gh-action-vpn" = {
+        user = "qbetter";
+        hostname = "192.168.154.184";
+      };
+      "qube-ssh" = {
+        user = "ec2-user";
+        hostname = "ssh.myqube.io"; # this will be changed soon to ssh.qube.q-better.com
+        identityFile = "~/.ssh/aws_admin_rsa";
+      };
+      "qube-dev-aux" = {
+        user = "ubuntu";
+        hostname = "ec2-3-248-205-52.eu-west-1.compute.amazonaws.com";
+        identityFile = "~/.ssh/aws_rsa";
+      };
+      "qube-preview" = {
+        user = "ec2-user";
+        hostname = "ec2-34-240-12-31.eu-west-1.compute.amazonaws.com";
+        identityFile = "~/.ssh/aws_admin_rsa";
+      };
+    };
+  };
+
   home.file = {
     ".config/MQTT-Explorer/settings.json" = {
       source = ./mqtt-explorer/settings.json;
