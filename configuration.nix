2c4c587c954a8efe416020cdaddf98b5305e7bd4
André Oliveira <p32blo@gmail.com>
2024-03-07 08:43:12 +0000

    add some programs

--- configuration.nix
+++ configuration.nix
@@ -109,7 +109,16 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    git
+    git 
+    du-dust
+    ripgrep
+    fd
+    zellij
+
+    blender
+    gimp
+
+    spotify
   ];
 
   virtualisation.virtualbox.guest.enable = true;
