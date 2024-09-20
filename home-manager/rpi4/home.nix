aa46e1fd0fbc69d87bd01bcaeb88d62797e61fe8
André Oliveira <p32blo@gmail.com>
2024-09-20 10:51:36 +0100

    Use a cursor theme

--- home-manager/rpi4/home.nix
+++ home-manager/rpi4/home.nix
@@ -14,8 +14,17 @@
   home.packages = with pkgs; [
     ungoogled-chromium
     alacritty
+    devenv
   ];
 
   home.stateVersion = "24.05";
   programs.home-manager.enable = true;
+
+  home.pointerCursor = {
+    x11.enable = true;
+    name = "DMZ-White";
+    size = 16;
+    package = pkgs.vanilla-dmz;
+    gtk.enable = true;
+  };
 }
