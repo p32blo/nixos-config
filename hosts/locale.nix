f67c3124c1ac973da243c275bd2cfd183ac7dcae
André Oliveira <p32blo@gmail.com>
2025-07-18 13:34:39 +0100

    Fix LC warnings on rpi4

--- hosts/locale.nix
+++ hosts/locale.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: {
+{...}: {
   # Set your time zone.
   time.timeZone = "Europe/Lisbon";
 
@@ -14,15 +9,8 @@
   i18n = {
     defaultLocale = "en_US.UTF-8";
     extraLocaleSettings = {
-      LC_ADDRESS = "pt_PT.UTF-8";
-      LC_IDENTIFICATION = "pt_PT.UTF-8";
-      LC_MEASUREMENT = "pt_PT.UTF-8";
-      LC_MONETARY = "pt_PT.UTF-8";
-      LC_NAME = "pt_PT.UTF-8";
-      #LC_NUMERIC = "pt_PT.UTF-8";
-      LC_PAPER = "pt_PT.UTF-8";
-      LC_TELEPHONE = "pt_PT.UTF-8";
-      #LC_TIME = "pt_PT.UTF-8";
+      LC_ALL = "pt_PT.UTF-8";
+      LC_CTYPE = "pt_PT.UTF-8";
     };
   };
 }
