bd6c4cac1ffa7df1d772d6e9fc5faee5073460cc
André Oliveira <p32blo@gmail.com>
2026-07-13 17:39:57 +0100

    Fix deprecation warning

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,7 +7,7 @@
 
   (final: prev: {
     unstable = import inputs.nixpkgs-unstable {
-      system = final.system;
+      system = final.stdenv.hostPlatform.system;
       config.allowUnfree = true;
     };
   })
